cd3ae7876261ce5627672f5e2cde9aa9e9fc9da0
guy-bartkus <guy.bartkus@gmail.com>
2023-10-27 21:03:08 -0700

    Day 2 Part 1

--- day2/p1.cpp
+++ day2/p1.cpp
@@ -18,9 +18,7 @@ int main() {
 	std::array<char, 2> search;
     int score = 0;
 
-	while (istream >> search[0] >> search[1]) {
-        score += results.find(search)->second;
-	}
+	while (istream >> search[0] >> search[1]) score += results.find(search)->second;
 
     std::cout << score << std::endl;
 }
