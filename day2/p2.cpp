270886b3db30c9d604c4be3a687097f319185fea
guy-bartkus <guy.bartkus@gmail.com>
2023-10-27 22:37:57 -0700

    Add Part 2

--- day2/p2.cpp
+++ day2/p2.cpp
@@ -9,9 +9,9 @@ int main() {
 	(istream.is_open()) ? 0 : (std::cout << "Failed to open file!" << std::endl, exit(1), 1);
 
 	std::map<std::array<char, 2>, uint8_t> results = {
-		{{'A','Y'}, 8}, {{'A','Z'}, 3}, {{'A','X'}, 4},
+		{{'A','Y'}, 4}, {{'A','Z'}, 8}, {{'A','X'}, 3},
 		{{'B','X'}, 1}, {{'B','Z'}, 9}, {{'B','Y'}, 5},
-		{{'C','X'}, 7}, {{'C','Y'}, 2}, {{'C','Z'}, 6}
+		{{'C','X'}, 2}, {{'C','Y'}, 6}, {{'C','Z'}, 7}
 	};
 
 	std::array<char, 2> search;
