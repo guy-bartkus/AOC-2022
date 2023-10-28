ed1e02cfc92e0c1affc92c843ac2f9ba3cd37f24
guy-bartkus <guy.bartkus@gmail.com>
2023-10-27 21:02:43 -0700

    Day 2 Part 1

--- day2/p1.cpp
+++ day2/p1.cpp
@@ -10,17 +10,16 @@ int main() {
 	(istream.is_open()) ? 0 : (std::cout << "Failed to open file!" << std::endl, exit(1), 1);
 
     std::map<std::array<char, 2>, uint8_t> results = {
-        {{'A','Y'}, 8}, {{'A','Z'}, 3}, {{'A','X'}, 3},
-        {{'B','X'}, 1}, {{'B','Z'}, 9}, {{'B','Y'}, 3},
-        {{'C','X'}, 7}, {{'C','Y'}, 2}, {{'C','Z'}, 3}
+        {{'A','Y'}, 8}, {{'A','Z'}, 3}, {{'A','X'}, 4},
+        {{'B','X'}, 1}, {{'B','Z'}, 9}, {{'B','Y'}, 5},
+        {{'C','X'}, 7}, {{'C','Y'}, 2}, {{'C','Z'}, 6}
     };
 
 	std::array<char, 2> search;
     int score = 0;
 
-	while (istream.peek() != EOF) {
-        istream >> search[0]; istream >> search[1];
-        score += (int)results.find(search)->second;
+	while (istream >> search[0] >> search[1]) {
+        score += results.find(search)->second;
 	}
 
     std::cout << score << std::endl;
